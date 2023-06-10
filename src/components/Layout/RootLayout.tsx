e2514dc2bd1e75e34835fc2804030bf192528f70
Ghaadyy <ghadyyi@gmail.com>
2023-06-10 17:41:15 +0300

    Added auto logout when JWT expires

--- src/components/Layout/RootLayout.tsx
+++ src/components/Layout/RootLayout.tsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import { NavBar } from "../Navigation/NavBar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { isExpired } from "react-jwt";
+import UserContext from "../../store/user-context";
 
 export const toastSuccess = (message: string) => toast.success(message);
 export const toastError = (message?: string) =>
   toast.error(message || "An error occured!");
 
 const RootLayout: React.FC = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const userCtx = useContext(UserContext);
+
+  const storedToken = localStorage.getItem("token");
+
+  useEffect(() => {
+    if (storedToken && isExpired(storedToken)) {
+      userCtx.logout();
+      navigate("/");
+    }
+  }, [navigate, userCtx, storedToken, location]);
+
   return (
     <>
       <NavBar />
