84ed5f4012c42f1c5d43d08b76addc9306bef33f
Ghaadyy <ghadyyi@gmail.com>
2023-06-10 18:35:15 +0300

    Added protection to certain routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,61 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  Navigate,
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
 import RootLayout from "./components/Layout/RootLayout";
 import LoginPage from "./pages/LoginPage";
 import TasksPage from "./pages/TasksPage";
 import CreateTaskPage from "./pages/CreateTaskPage";
 import SignUpPage from "./pages/SignUpPage";
 import EditTaskPage from "./pages/EditTaskPage";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <RootLayout />,
-    children: [
-      {
-        index: true,
-        element: (
-          <div className="w-full flex flex-col items-center justify-center h-52">
-            <h1 className="font-bold text-2xl">Welcome to Task Manager!</h1>
-          </div>
-        ),
-      },
-      {
-        path: "login",
-        element: <LoginPage />,
-      },
-      {
-        path: "signup",
-        element: <SignUpPage />,
-      },
-      {
-        path: "tasks",
-        element: <TasksPage />,
-      },
-      {
-        path: "create-task",
-        element: <CreateTaskPage />,
-      },
-      {
-        path: "edit-task/:id",
-        element: <EditTaskPage />,
-      },
-    ],
-  },
-]);
+import { useContext } from "react";
+import UserContext from "./store/user-context";
 
 const App: React.FC = () => {
+  const userCtx = useContext(UserContext);
+
+  const protectRoute = (child: React.ReactNode): React.ReactNode => {
+    return userCtx.isLoggedIn ? child : <Navigate to="/login" />;
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <RootLayout />,
+      children: [
+        {
+          index: true,
+          element: (
+            <div className="w-full flex flex-col items-center justify-center h-52">
+              <h1 className="font-bold text-2xl">Welcome to Task Manager!</h1>
+            </div>
+          ),
+        },
+        {
+          path: "login",
+          element: <LoginPage />,
+        },
+        {
+          path: "signup",
+          element: <SignUpPage />,
+        },
+        {
+          path: "tasks",
+          element: protectRoute(<TasksPage />),
+        },
+        {
+          path: "create-task",
+          element: protectRoute(<CreateTaskPage />),
+        },
+        {
+          path: "edit-task/:id",
+          element: protectRoute(<EditTaskPage />),
+        },
+      ],
+    },
+  ]);
+
   return <RouterProvider router={router} />;
 };
 
