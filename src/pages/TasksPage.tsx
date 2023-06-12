842a0cd3c01075e0fdf9ed47a69c6443e5314d52
Ghaadyy <ghadyyi@gmail.com>
2023-06-12 20:48:10 +0300

    Added support for touch backend dnd

--- src/pages/TasksPage.tsx
+++ src/pages/TasksPage.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import TaskGrid from "../components/Tasks/TaskGrid";
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
+// import { DndProvider } from "react-dnd";
+// import { HTML5Backend } from "react-dnd-html5-backend";
+import { DndProvider } from "react-dnd-multi-backend";
+import { HTML5toTouch } from "rdndmb-html5-to-touch";
 
 const TasksPage = () => {
   return (
     <div className="flex items-center justify-center">
-      <DndProvider backend={HTML5Backend}>
+      <DndProvider options={HTML5toTouch}>
         <TaskGrid />
       </DndProvider>
     </div>
